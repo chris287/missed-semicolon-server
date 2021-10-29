5ad6e1b4a67a99a5e434462b1d05682f4c4bf6ed
christopherfernandes <cfernandes@deloitte.com>
2021-10-29 20:48:37 +0530

    10/29

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors'); 
 
 var newsRouter = require('./routes/news');
 
@@ -11,12 +12,14 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('port', process.env.PORT || 3000);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/news', newsRouter);
 
